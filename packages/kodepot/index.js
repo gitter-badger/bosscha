d171773307b9a05c85bb19c5d14067d572196635
Alif Rachmawadi <code@subosito.com>
2016-01-31 23:00:13 +0700

    added hashids

--- packages/kodepot/index.js
+++ packages/kodepot/index.js
@@ -1,1 +1,11 @@
 hljs = Npm.require("highlight.js");
+huid = function(key) {
+  if (key === undefined) {
+    key = Meteor.uuid();
+  }
+
+  var Hashids = Npm.require('hashids');
+  var uuid = new Hashids(key, 6);
+
+  return uuid.encode(1001);
+}
